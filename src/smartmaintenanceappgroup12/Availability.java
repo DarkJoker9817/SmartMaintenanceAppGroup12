7052c78b87e9d173cd95b2df7275469bf9ec99c1
UgoBarbato <ugobarbato@icloud.com>
2020-11-21 17:51:31 +0100

    Modificato class User Aggiunto class Planner, Maintainer, Competence e Availability

---
+++ src/smartmaintenanceappgroup12/Availability.java
@@ -0,0 +1,26 @@
+package smartmaintenanceappgroup12;
+
+import java.util.Map;
+
+/**
+ *
+ * @author ugobarbato
+ */
+public class Availability {
+    
+    public enum WorkDay { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY,
+                            SATURDAY, SUNDAY }
+    
+    private Map<WorkDay,String> percentage;
+    private Map<WorkDay,String> hour;
+
+    public Map<WorkDay, String> getPercentage() {
+        return percentage;
+    }
+
+    public Map<WorkDay, String> getHour() {
+        return hour;
+    }
+    
+    
+}
--- src/smartmaintenanceappgroup12/User.java
+++ src/smartmaintenanceappgroup12/User.java
@@ -4,6 +4,21 @@ package smartmaintenanceappgroup12;
  *
  * @author ugobarbato
  */
-public class User {
+public abstract class User {
+    private String username;
+    private String password;
+    
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
     
 }
