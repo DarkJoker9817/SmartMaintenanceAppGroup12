425ab33cacfc4ecb9c70a6bf9649b0eef6e7982c
gioca <gioca@LAPTOP-ADKTP1IE.homenet.telecomitalia.it>
2020-11-23 17:34:47 +0100

    Modificato SysAdminGUI

--- src/smartmaintenanceappgroup12/SysAdminGUI.java
+++ src/smartmaintenanceappgroup12/SysAdminGUI.java
@@ -28,6 +28,7 @@ public class SysAdminGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("System Administrator");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
