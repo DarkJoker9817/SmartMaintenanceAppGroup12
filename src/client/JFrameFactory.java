432e2a0fc6693e34de95c1710dd1898954117fa8
gioca <gioca@LAPTOP-ADKTP1IE.homenet.telecomitalia.it>
2020-11-27 19:45:26 +0100

    JFrame factory

---
+++ src/client/JFrameFactory.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author gioca
+ */
+public class JFrameFactory {
+
+    public JFrame getJFrame(String gui) {
+        if (gui.equals("System Administrator")) {
+            return new SysAdminGUI();
+        }
+        if (gui.equals("Planner")) {
+            return new PlannerGUI();
+        }
+        if (gui.equals("Maintainer")) {
+            return new MaintainerGUI();
+        }
+        return null;
+    }
+
+}
