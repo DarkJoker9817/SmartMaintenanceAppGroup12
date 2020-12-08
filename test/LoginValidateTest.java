1b705bb594a550efcad82a367ac9e9a57032b394
gioca <gioca@LAPTOP-ADKTP1IE.homenet.telecomitalia.it>
2020-12-08 16:20:43 +0100

    Revert "test"
    This reverts commit b696c6966b02287aee932058c189e9b1c9b6e4bc.

--- test/LoginValidateTest.java
+++ test/LoginValidateTest.java
@@ -55,4 +55,9 @@ public class LoginValidateTest {
         rep.delete("delete from sys_admin");
     }
 
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
 }
