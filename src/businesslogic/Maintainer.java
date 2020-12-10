fc9a2b0c64205a6ef85a7e11b78827a6a869df85
gioca <gioca@LAPTOP-ADKTP1IE.homenet.telecomitalia.it>
2020-12-10 16:20:02 +0100

    Rimossa classe Availability

--- src/businesslogic/Maintainer.java
+++ src/businesslogic/Maintainer.java
@@ -10,7 +10,6 @@ public class Maintainer extends User {
 
     private List<String> competences;
     private int activityID;
-    private Availability availability;
 
     public List<String> getCompetences() {
         return competences;
@@ -20,20 +19,12 @@ public class Maintainer extends User {
         return activityID;
     }
 
-    public Availability getAvailability() {
-        return availability;
-    }
-
     public void addCompetence(String competence) {
         this.competences.add(competence);
     }
 
     public void setActivityID(int activityID) {
         this.activityID = activityID;
-    }
-
-    public void setAvailability(Availability availability) {
-        this.availability = availability;
     }
 
 }
